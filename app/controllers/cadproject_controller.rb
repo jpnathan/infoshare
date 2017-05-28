7a837de7712e83fc2cfe089cd5307e39191f9e5d
phillip18 <phillipjonathan@gmail.com>
2017-05-28 01:00:26 -0300

    continues cadproject

--- app/controllers/cadproject_controller.rb
+++ app/controllers/cadproject_controller.rb
@@ -1,42 +1,47 @@
 class CadprojectController < ApplicationController
-  before_action :find_item, only: [:index, :edit, :show, :update, :destroy]
+  before_action :find_item, only: [:edit, :show, :update, :destroy]
+  #before_action :authenticate_user!, except: [:index, :show]
   
   def index
+    @projects = CadProjeto.all.order('created_at DESC')
+    #if user_signed_in?
+    #  @project = CadProjeto.where(:user_id => current_user.id).order("created_at DESC") 
+    #end
   end
 
-    def new
-      @project = CadProjeto.new
-    end
+  def new
+    @project = CadProjeto.new
+  end
 
-    def create
-      @project = CadProjeto.new(cadproject_params)
+  def create
+    @project = CadProjeto.new(cadproject_params)
 
-      if @project.save
-        redirect_to @project
-      else
-        render 'new'
-      end
-    end
-
-    def show
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-    def destroy
-    end
-
-    private
-
-    def cadproject_params
-      params.require(:cad_projetos).permit(:nome, :autor, :categoria, :descricao, :id)
-    end
-
-    def find_item
-      @project = CadProjeto.find(params[:id])
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
     end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def cadproject_params
+    params.require(:cadproject).permit(:nome, :autor, :categoria, :descricao, :id)
+  end
+
+  def find_item
+    @project= CadProjeto.find(params[:id])
+  end
+end
