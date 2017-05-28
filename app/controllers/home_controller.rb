18f789de7c26c932c0b9e3b4ef7b0b20d3ca0f50
phillip18 <phillipjonathan@gmail.com>
2017-05-28 11:15:13 -0300

    Add posts at home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
 	before_action :authenticate_user!
+
+	def index
+		@accomodations = Accommodation.all.order('created_at DESC')
+		@jobs = Job.all.order('created_at DESC')
+		@projects = Project.all.order('created_at DESC')
+		@transports = Transport.all.order('created_at DESC')
+	end
+	def load
+		Rails.application.eager_load!
+	end
 end
