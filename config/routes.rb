abdea2f172312b20d86463271ea655b66520460b
phillip18 <phillipjonathan@gmail.com>
2017-05-28 08:04:19 -0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-  root 'home#index'
-
-  devise_for :users
-  resources :projects
-  resources :jobs
-  resources :accommodations
-  resources :transports
-  
-=======
 
 	root 'home#index'
+	
 	devise_for :users
 	resources :projects
 	resources :jobs
 	resources :accommodations
+	resources :transports
+	
 	get '/biblioteca' => 'pages#biblioteca'
 	get '/grupos' => 'pages#grupos'
 
->>>>>>> 5b418457b834377884c60ed83da34cbe1d108cc6
 end
